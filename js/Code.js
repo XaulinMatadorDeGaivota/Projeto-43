ed79b89cb238ee2da352a3fed8b3a4b1e3c53db7
Matheus Cabral Calil <93673317+XaulinMatadorDeGaivota@users.noreply.github.com>
2022-05-01 20:58:36 -0300

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,16 @@
+const accessCode1 = "VARIÁVEL";
+const accessCode2 = "FUNÇÃO";
+const accessCode3 = "BANCO DE DADOS";
+
+function clues() {
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: Sempre mudando, não constante!", 100, 70);
+
+  // Adicione código para exibir as outras duas perguntas.
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: Perdorma uma tarefa em particular", 650, 170);
+}
